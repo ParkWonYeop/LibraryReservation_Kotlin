a2b0e51d98fa1ae1ae88fc02cbafc37c1bbcf283
ParkWonYeop <pwy9679@gnu.ac.kr>
2024-06-03 13:36:20 +0900

    main/2024.06.03
    - CustomPageRequest 구현
    - Test schema.sql DROP TABLE => TRUNCATE 변경
    - 서비스 레이어가 아닌 DTO 내부에서 Null check 하는 Custom getter 구현

--- src/main/kotlin/com/example/libraryReservationKotlin/common/repository/RoomRepository.kt
+++ src/main/kotlin/com/example/libraryReservationKotlin/common/repository/RoomRepository.kt
@@ -6,6 +6,5 @@ import com.example.libraryReservationKotlin.common.pageable.CustomPageRequest
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface RoomRepository : JpaRepository<RoomEntity, Long> {
-    fun findByRoomType(customPageRequest: CustomPageRequest, roomEnum: RoomEnum): List<RoomEntity>
     fun findByRoomType(roomEnum: RoomEnum): List<RoomEntity>
 }
